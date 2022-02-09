7331079f9dff99f090dfacaad023b50241898acd
Johannes Skøien <johannes_skoien@hotmail.com>
2022-02-09 15:04:00 +0100

    Add file-attachments

--- EmailSender.py
+++ EmailSender.py
@@ -1,19 +1,51 @@
 import os
 import smtplib
+import imghdr
 from email.message import EmailMessage
 
-# Email and password saved as environment variables locally.
+##Email and password saved as environment variables locally.##
 user = os.environ.get('NOTTER_USER')
 password = os.environ.get('NOTTER_PASS')
 
-reciever = ""  # Dummy reciever
+##Dummy recipient##
+reciever = "test@testmail.com"
 
-# Setup message
+##Setup message##
 msg = EmailMessage()
 msg['Subject'] = 'Email Subject'
 msg['From'] = user
 msg['To'] = reciever
 msg.set_content('Email Body')
+
+##FILE ATTACHMENT##
+with open('test.jpg', 'rb') as f:
+
+    ##.JPG##
+    file_data = f.read()
+    file_type = imghdr.what(f.name)
+    file_name = f.name
+
+    ##.PDF##
+    # file_data = f.read()
+    # file_name = f.name
+
+##IMAGE##
+msg.add_attachment(file_data, maintype='image',
+                   subtype='file_type', filename=file_name)
+
+##PDF##
+# msg.add_attachment(file_data, maintype='application',
+#                    subtype='octet-stream', filename=file_name)
+
+##HTML##
+msg.add_alternative("""\
+    <!DOCTYPE html>
+    <html>
+        <body>
+            <h1 style="color:SlateGray;">HTML Test</h1>
+        </body>
+    </html>
+""", subtype='html')
 
 ##WITH SSL##
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
